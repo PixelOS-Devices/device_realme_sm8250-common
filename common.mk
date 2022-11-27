43a3e576557cb7525dbb882ad4661d6a1bdca7ed
Mashopy <eliasgheeraert@gmail.com>
2022-11-27 23:22:37 +0100

    sm8250-common: Set GENERIC_ODM_IMAGE to true
    This flag allow QCOM media hal to set debug.stagefright.ccodec to 4 instead of 1 which match RealmeUI ccodec property

--- common.mk
+++ common.mk
@@ -257,6 +257,8 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.oplus
 
 # Media
+GENERIC_ODM_IMAGE := true
+
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
@@ -271,6 +273,8 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor
+
+
 
 # Net
 PRODUCT_PACKAGES += \
