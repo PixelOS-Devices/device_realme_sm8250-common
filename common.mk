AOSPA/android_device_realme_sm8250-common

sm8250-common: audio: Create another audio policy configuration for BLE Audio supported devices
The Snapdragon 865+ 5G and Snapdragon 870 5G support the bluetooth version 5.2 [1 & 2] unlike the Snapdragon 865 5G who only support the bluetooth version 5.1 [3].
The version 5.2 of the Bluetooth Core Specification is required for BLE Audio codec like LC3 [4], hence using a different audio policy configuration is required to avoid adding LC3 on non-supported kona.

For realme SM8250 case, only the realme X50 Pro 5G and the realme X50 Pro Player use the Snapdragon 865 5G while the rest use the Snapdragon 870 5G.

The audio policy configuratoin for BLE Audio will replace the non BLE Audio one only when TARGET_USES_BLUETOOTH_LE_AUDIO is set to true.

[1]: https://www.qualcomm.com/products/mobile/snapdragon/smartphones/snapdragon-8-series-mobile-platforms/snapdragon-865-plus-5g-mobile-platform[2]: https://www.qualcomm.com/products/mobile/snapdragon/smartphones/snapdragon-8-series-mobile-platforms/snapdragon-865-5g-mobile-platform
[2]: https://www.qualcomm.com/products/mobile/snapdragon/smartphones/snapdragon-8-series-mobile-platforms/snapdragon-870-5g-mobile-platform
[3]: https://www.qualcomm.com/products/mobile/snapdragon/smartphones/snapdragon-8-series-mobile-platforms/snapdragon-865-5g-mobile-platform
[4]: https://www.bluetooth.com/media/le-audio/le-audio-faqs

Change-Id: Ibc7e145635c419d72523159e8d76b1220ab33c49

common.mk
@@ -28,6 +28,11 @@ PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
 
 # Audio
+ifeq ($(TARGET_USES_BLUETOOTH_LE_AUDIO), true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_DIR)/audio/audio_policy_configuration_ble_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml
+endif
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_ODM)/etc/audio_io_policy.conf \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \

PixelOS-Devices/device_realme_sm8250-common

common.mk
@@ -74,6 +74,11 @@ PRODUCT_PACKAGES += \
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8250/audio
 
+ifeq ($(TARGET_USES_BLUETOOTH_LE_AUDIO), true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration_ble_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml
+endif
+
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/bluetooth_qti_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_hearing_aid_audio_policy_configuration.xml \
     $(AUDIO_HAL_DIR)/configs/kona/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \