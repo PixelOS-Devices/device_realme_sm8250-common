d21d518dd8470221d31e845f135ce4f15e3e93f3
cristianvaz <cristianvaz81993@gmail.com>
2023-05-10 15:09:53 +0200

    Revert "sm8250-common: Drop non-existent modules in PRODUCT_PACKAGES"
    This reverts commit 0a6d35a5f83062324ce5a8c4ce857097eab198c6.

--- common.mk
+++ common.mk
@@ -53,6 +53,7 @@ PRODUCT_PACKAGES += \
     libdynproc \
     libeffectproxy \
     libhdmiedid \
+    libhdmipassthru \
     libhfp \
     libldnhncr \
     libqcompostprocbundle \
@@ -109,6 +110,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default
 
+PRODUCT_PACKAGES += \
+    libbthost_if
+
 # Bootanimation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2400
@@ -157,6 +161,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
     libqdMetaData \
     libsdmcore \
     libsdmutils \
@@ -200,7 +205,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@2.1.vendor
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
@@ -449,6 +456,10 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     libwpa_client \
     wpa_supplicant.conf
+
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay \
+    WifiOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
